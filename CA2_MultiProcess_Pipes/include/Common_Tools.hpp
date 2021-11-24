5106c5a8d64a5db31f3f7f1236716b2137ac0155
Moein Karami <moein2000n@gmail.com>
2021-11-24 19:41:39 +0330

    common tools done

--- CA2_MultiProcess_Pipes/include/Common_Tools.hpp
+++ CA2_MultiProcess_Pipes/include/Common_Tools.hpp
@@ -6,7 +6,7 @@
 class CommonTools
 {
 	public:
-		static std::vector<std::string> divide_by();
+		static std::vector<std::string> divide_by(std::string data, char divider);
 		static std::string convert_key_value_map_to_string(KeyValueMap key_values);
 		static KeyValueMap convert_string_to_key_value_map(std::string data);
 };
---
+++ CA2_MultiProcess_Pipes/src/Common_Tools.cpp
@@ -0,0 +1,38 @@
+#include "Common_Tools.hpp"
+
+std::vector<std::string> CommonTools::divide_by(std::string data, char divider)
+{
+	std::vector<std::string> words;
+	std::string word;
+	for (auto letter : data)
+	{
+		if (letter == divider && word.size())
+		{
+			words.push_back(word);
+			word.clear();
+		}
+		else
+			word += letter;
+	}
+	return words;
+}
+
+std::string CommonTools::convert_key_value_map_to_string(KeyValueMap key_value)
+{
+	std::string data;
+	for (auto element : key_value)
+		data += element.key + std::to_string(element.value) + COMMA;
+	if (data.size())
+		data.pop_back();
+	return data;
+}
+
+KeyValueMap CommonTools::convert_string_to_key_value_map(std::string data)
+{
+	KeyValueMap key_value;
+	std::vector<std::string> words = divide_by(data, COMMA);
+	for (int i = 0 ; i < words.size(); i++)
+		key_value[words[i]] += std::stoi(words[i+1]);
+	return key_value;
+}
+
